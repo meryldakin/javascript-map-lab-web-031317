db1d4f7d9636b646c97a201be01020d52d0205ee
meryldakin <meryl.k.dakin@gmail.com>
2017-04-25 16:26:16 -0400

    Done.

--- index.js
+++ index.js
@@ -21,3 +21,19 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null }
 ];
+
+var sortedRobots = robots.map(function(element) {
+  if (knownDecepticons.includes(element.name)) {
+    return Object.assign( {}, element, {alliance: "decepticon"} )
+  } else {
+    return Object.assign( {}, element, {alliance: "autobot"} )
+  }
+})
+
+var coloredZebraStripes = zebraStripes.map(function(element, index) {
+  if (index % 2 === 0) {
+    return Object.assign( {}, element, {color: "black"})
+  } else {
+    return Object.assign( {}, element, {color: "white"})
+  }
+})
